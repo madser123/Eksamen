84603e4a1b81f841fb1c6da5562ed3b1f1edcfa0
therealbangsen <lucas.bang.ingvartsen@gmail.com>
2019-04-08 09:42:27 +0200

    loader ting

--- loader/loader.js
+++ loader/loader.js
@@ -1,1 +1,24 @@
-
+var loader = document.querySelector('.loader');
+var loadingAnim = new Moveit(loader, {
+  start: '0%',
+  end: '0%'
+});
+function animate() {
+  loadingAnim.set({
+    start: '0%',
+    end: '70%',
+    duration: 0.5,
+    callback: function() {
+      loadingAnim.set({
+        start: '100%',
+        end: '101%',
+        duration: 0.8,
+        follow: true,
+        callback: function() {
+          animate();
+        }
+      });
+    }
+  })
+}
+animate();
